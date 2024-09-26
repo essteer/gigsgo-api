4e39740a8493464b132879d8306e1b2fe8e20062
essteer <essteer@pm.me>
2024-09-26 22:50:10 +0800

    feat: Add placeholders for genre and day selection

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      notoSerif: ["Noto Serif"],
       oswald: ["Oswald"],
+      zcool: ["ZCOOL QingKe HuangYou"]
     }
   },
   plugins: [],
